d29d6c78e881e223d68ba351265686201a9c479e
Snezhnyy <anatoly.dobshikov@gmail.com>
2018-04-26 17:11:34 +0300

    init

--- Program.cs
+++ Program.cs
@@ -1,12 +1,60 @@
-﻿using System;
+﻿// Hello, Vasya
 
-namespace vasya_v2
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.ReplyMarkups;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+
+namespace Telegram.Bot.vasya_bot
 {
     class Program
     {
+        private static TelegramBotClient Bot;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Bot = new TelegramBotClient(ReadToken());
+            var me = Bot.GetMeAsync().Result;
+            Console.Title = me.Username;
+
+            Console.WriteLine(System.Reflection.Assembly.GetExecutingAssembly().Location);
+
+            Bot.OnMessage += BotOnMessageReceived;
+
+            Bot.StartReceiving(Array.Empty<UpdateType>());
+            Console.WriteLine($"Start listening for @{me.Username}");
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+            Bot.StopReceiving();
+        }
+
+        private static async void BotOnMessageReceived(object sender, MessageEventArgs messageEventArgs)
+        {
+            var message = messageEventArgs.Message;
+            if (message == null || message.Type != MessageType.TextMessage) return;
+            string answer = "";
+            switch (message.Text)
+            {
+                default:
+                    answer = @"Looking good!";
+                    break;     
+            }  
+            await Bot.SendTextMessageAsync(
+                        message.Chat.Id,
+                        answer,
+                        replyMarkup: new ReplyKeyboardRemove());
+        }
+
+        private static string ReadToken()
+        {
+            return JsonConvert.DeserializeObject<Dictionary<string, string>>(File.ReadAllText("bot_passport.json"))["token"];
         }
     }
 }
