4ccb460b9242705c200726082f1641d0c0991bbb
Peter Zhukovets <mrzhukovets@gmail.com>
2018-04-27 11:29:08 +0300

    Добавлена реплика DeusVult в ответ на Иерусалим

---
+++ lib/DeusVult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Telegram.Bot.Args;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types.InlineQueryResults;
+using Telegram.Bot.Types.ReplyMarkups;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace Telegram.Bot.vasya_v2
+{
+    class DeusVult : DefaultResponse
+    {
+
+        public CrackPentagon(MessageEventArgs message) : base(message)
+        {
+        }
+
+        public override string Reply()
+        {
+            return "DEUS VULT!";
+        }
+    }
+}
