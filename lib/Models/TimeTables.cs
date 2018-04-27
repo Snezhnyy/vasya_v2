2a3acd97bc0ab2a66fd7b4dc231479ee0ae31a41
Xaranna <vega_21_21@mail.ru>
2018-04-27 11:37:46 +0300

    Добавлена модель БД

---
+++ lib/Models/TimeTables.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vasya_v2.lib.Models
+{
+    public class TimeTables
+    {
+        public int TmTb_ID { set; get; }
+        public string TmTb_DayWeek { set; get; }
+        public string TmTb_NameSbj { set; get; }
+        public int TmTb_Order { set; get; }
+        public string TmTb_Cabinet { set; get; }
+    }
+}
